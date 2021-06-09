a5d141268cba011f150da12c4254949ee0530635
Kenneth Eberhardt Mølbak <keb@jdm.dk>
2021-06-09 15:18:31 +0200

    test

--- src/java/eu/sig/training/ch03/binarytree/BinaryTreeSearch.java
+++ src/java/eu/sig/training/ch03/binarytree/BinaryTreeSearch.java
@@ -1,8 +1,6 @@
 package eu.sig.training.ch03.binarytree;
 
 public class BinaryTreeSearch {
-
-    // tag::calculateDepth[]
     public static int calculateDepth(BinaryTreeNode<Integer> node, int nodeValue) {
         int depth = 0;
         if (node.getValue() == nodeValue) {
@@ -25,6 +23,4 @@ public class BinaryTreeSearch {
             }
         }
     }
-    // end::calculateDepth[]
-
 }
