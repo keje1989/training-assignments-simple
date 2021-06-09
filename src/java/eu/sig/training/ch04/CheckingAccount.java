3e88c709d06da42e779ee52facba13bfca4fa14e
Kenneth Eberhardt Mølbak <keb@jdm.dk>
2021-06-09 12:01:59 +0200

    test

--- src/java/eu/sig/training/ch04/CheckingAccount.java
+++ src/java/eu/sig/training/ch04/CheckingAccount.java
@@ -1,7 +1,7 @@
 package eu.sig.training.ch04;
 
 // tag::CheckingAccount[]
-public class CheckingAccount {
+public class CheckingAccount extends Accounts {
     private static final float INTEREST_PERCENTAGE = 0.01f;
     private Money balance = new Money();
     private int transferLimit = 100;
@@ -13,13 +13,8 @@ public class CheckingAccount {
             throw new BusinessException("Limit exceeded!");
         }
         // 2. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0;
-        for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            sum = sum + (9 - i) * characterValue;
-        }
-        if (sum % 11 == 0) {
+        
+        if (isValid(counterAccount)) {
             // 3. Look up counter account and make transfer object:
             CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
             Transfer result = new Transfer(this, acct, amount);
